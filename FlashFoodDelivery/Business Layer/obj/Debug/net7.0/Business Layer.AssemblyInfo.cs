0592a5141697c8355605d5f0c6f96093a9c343a5
Phuoc Thinh <phuocthinhlkr@gmail.com>
2024-05-20 09:33:06 +0700

    added gitignore, services and repositories classes

--- FlashFoodDelivery/Business Layer/obj/Debug/net7.0/Business Layer.AssemblyInfo.cs
+++ FlashFoodDelivery/Business Layer/obj/Debug/net7.0/Business Layer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business Layer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ffbcd81ccbefa875731b1a72af8b8d56323e30c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business Layer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business Layer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
