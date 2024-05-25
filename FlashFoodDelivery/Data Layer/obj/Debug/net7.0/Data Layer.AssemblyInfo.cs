052d5345bfc66953fad234e2f06c46969d2bc5b2
Khang Duy <duyktkse161141@fpt.edu.vn>
2024-05-25 18:58:57 +0700

    duy commit

--- FlashFoodDelivery/Data Layer/obj/Debug/net7.0/Data Layer.AssemblyInfo.cs
+++ FlashFoodDelivery/Data Layer/obj/Debug/net7.0/Data Layer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data Layer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d788004206c2caf5acfbdea83ff3c77d95eefe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data Layer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data Layer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
