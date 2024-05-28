dd5ab5f39ef78f59734f182cd42e6a0f3b9d6b34
Khang Duy <duyktkse161141@fpt.edu.vn>
2024-05-28 16:19:10 +0700

    Merge DuyktkVer2

--- FlashFoodDelivery/Data Layer/obj/Debug/net7.0/Data Layer.AssemblyInfo.cs
+++ FlashFoodDelivery/Data Layer/obj/Debug/net7.0/Data Layer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data Layer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c2c28bc57a99baafd9579a7e275a2ff30d0fad5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6b01ea55d575e4ef2f4f911badbb85b97bd3fe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data Layer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data Layer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
